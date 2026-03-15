d7355b86603552bc6de34ba24923df7d837fe12f
test <test@example.com>
2026-03-15 21:59:53 +0500

    first steps

--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ import time #noqa
 import asyncio #noqa
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
-
-app = FastAPI()
-app.add_middleware(CORSMiddleware,allow_origins = "*")
-
+import uvicorn #noqa
+from src.api.CRUD import AsyncCREATE
+async def main():
+    app = FastAPI()
+    app.add_middleware(CORSMiddleware,allow_origins = "*")
+    
+    @app.post("Compliment/CreateBase")
+    async def AsCreate():
+        result = AsyncCREATE.create_tables()
+        await result
 
 if __name__ == "__main__":
-    # asyncio.run(main())
-    uvicorn.run(app = "compliment_app.main:app",host="0.0.0.0",reload=True,port=8000)
+    asyncio.run(main())
+    uvicorn.run(app = "src.main:app",host="0.0.0.0",reload=True,port=8000)
