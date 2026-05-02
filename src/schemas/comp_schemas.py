83ce130bd8a571c1ba626e68bae61a7c57e5ca88
test <test@example.com>
2026-05-02 22:10:43 +0500

    fix schemas

--- src/schemas/comp_schemas.py
+++ src/schemas/comp_schemas.py
@@ -1,13 +1,21 @@
 # Models for validation
+from datetime import datetime
 from pydantic import BaseModel, ConfigDict
 from typing import Optional
 from src.models.comp_models import Gender
 
 class ComplimentResponse(BaseModel):
+    # as i know from_attributes might read field from objects attributes
     model_config = ConfigDict(from_attributes = True)
     title:str
     point:Optional[str]
+
+class ComplimentHistoryResponse(BaseModel):
+    model_config = ConfigDict(from_attributes= True)
+    compliment:Optional[ComplimentResponse]
+    created_at:datetime
 class ComplimentSchema(BaseModel):
+    model_config = ConfigDict(from_attributes= True)
     title:str
     gender:Optional[Gender|None]
     point:str|None
