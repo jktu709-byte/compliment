ca6e38b7b6aa9de96cd74d2c430919186e067a06
test <test@example.com>
2026-09-17 08:05:41 +0500

    im stupid

--- src/exceptions/handlers.py
+++ src/exceptions/handlers.py
@@ -1,6 +1,12 @@
 # ХЭНДЛЕР НУЖЕН ДЛЯ ПРАВИЛЬНОГО СООТНОШЕНИЯ СЕМАНТИЧЕСКИХ, ТЕХНИЧЕСКИХ ОЩИБОК И ВОЗВРАЩЕНИЯ ВЕРНОГО ОТВЕТА ПОЛЬЗОВАТЕЛЮ И РАЗРАБУ
-from collections.abc import Callable, Coroutine #noqa
 from functools import wraps
+
+from fastapi import Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
+from src.exceptions.semantic_exceptions import AppError
+
 
 # чтож начало положено. ну я и лох конечно
 class ExceptionHandler:
@@ -9,18 +15,20 @@ class ExceptionHandler:
         pass
     
     # отлов базовых ошибок
-    def app_error_handler(self,func:Callable): # type: ignore
-        @wraps(func)
-        def wrapper():
-             res = func()
-             return res
-        return wrapper
+    @staticmethod
+    async def app_error_handler(request: Request,exc: AppError,) -> JSONResponse: ... # type: ignore
+        # @wraps(func)
+        # def wrapper():
+        #      res = func()
+        #      return res
+        # return wrapper
     
-    def validation_error_handler(self,func):
-        @wraps(func)
-        def wrapper():
-            res = func()
-            return res
-        return wrapper
+    @staticmethod
+    async def validation_error_handler(request: Request,exc: RequestValidationError) -> JSONResponse: ...
+        # @wraps(func)
+        # def wrapper():
+        #     res = func()
+        #     return res
+        # return wrapper
     
     
